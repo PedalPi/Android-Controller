807a7f9cb8feefe7b08dc72eb454efc9465e8319
SrMouraSilva <mateus.moura@hotmail.com>
2018-02-15 12:22:41 -0300

    Issue #3 Refator to extract android connection: Created Target abstraction

--- android_controller.py
+++ android_controller.py
@@ -1,25 +1,22 @@
 from application.component.component import Component
 
-from android_controller.adb import Adb
 from android_controller.android_controller_client import AndroidControllerClient
 from android_controller.request_message_processor import RequestMessageProcessor
 
 
 class AndroidController(Component):
     port = 8888
-    activity = 'io.github.pedalpi.pedalpi_display/io.github.pedalpi.pedalpi_display.MainActivity'
 
-    def __init__(self, application, ws_port, adb_command="adb"):
+    def __init__(self, application, target, ws_port=3000):
         """
         :param Application application: that AndroidController will be executed
-        :param int ws_port:
-        :param string adb_command: Command that call the Android Debug Bridge
-                                   In Raspberry maybe be a `./adb` executable file
+        :param Target target: Device protocol expected to communication
+        :param int ws_port: WebService port
         """
         super(AndroidController, self).__init__(application)
 
+        self.target = target
         self._client = AndroidControllerClient('localhost', AndroidController.port)
-        self.adb = Adb(adb_command, application.log)
         self.request_message_processor = RequestMessageProcessor(ws_port)
 
     def init(self):
@@ -28,12 +25,12 @@ class AndroidController(Component):
 
         self.request_message_processor.processed_listener = self._on_processed
 
-        self.adb.start(AndroidController.port, AndroidController.activity)
+        self.target.init(self.application, AndroidController.port)
         self._client.connect()
 
     def close(self):
         self.request_message_processor.close()
-        self.adb.close(AndroidController.port)
+        self.target.close()
 
     def _on_connected(self):
         self.application.log('AndroidController - DisplayView connected')
@@ -46,4 +43,5 @@ class AndroidController(Component):
     def _on_processed(self, request_message, response_message):
         self.application.log('AndroidController - Message sent: {}', response_message)
 
+        response_message = self.target.process(request_message, response_message)
         self._client.send(response_message)
