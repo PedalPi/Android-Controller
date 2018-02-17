04b209da91c8aa2056f1f30f03509d7f142de69f
SrMouraSilva <mateus.moura@hotmail.com>
2018-02-16 22:47:35 -0300

    FIX #10 - License Apache

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2017 SrMouraSilva
+# Copyright 2018 SrMouraSilva
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
